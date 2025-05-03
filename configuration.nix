1d817db3f6cab597cd427926d2a8819feb871d2c
Christian Döring <christian.doering@tum.de>
2025-05-03 12:50:21 +0200

    Added syncthing

--- configuration.nix
+++ configuration.nix
@@ -195,4 +195,11 @@
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
   };
+
+  # Syncthing
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+  };
+  systemd.services.syncthing.environment.STNODEFAULTFOLDER = "true"; # Don't create default ~/Sync folder
 }
