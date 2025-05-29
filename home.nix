e9c9d4e742f4a660aaa3d7e0e61adde779d75b37
Christian Döring <christian.doering@tum.de>
2025-05-29 22:36:43 +0200

    Added brave browser

--- home.nix
+++ home.nix
@@ -84,6 +84,18 @@
 
   programs.bottom.enable = true;
 
+  # Brave Browser
+  programs.chromium = {
+    enable = true;
+    package = pkgs.brave;
+    extensions = [
+      {id = "nngceckbapebfimnlniiiahkandclblb";} # bitwarden
+    ];
+    commandLineArgs = [
+      "--disable-features=WebRtcAllowInputVolumeAdjustment"
+    ];
+  };
+
   dconf = {
     enable = true;
 
