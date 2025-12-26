ffc6d012ca974b0445a357d3cdb2a51cd829da53
Christian Döring <christian.doering@tum.de>
2025-12-26 10:42:26 +0100

    Added dotfiles

--- sync.sh
+++ sync.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+# Goto the directory this executable is located in
+PROJECT_DIR=$(dirname "$(realpath $0)")
+cd $PROJECT_DIR
+
 export NIX_CONFIG="experimental-features = nix-command flakes"
 sudo nixos-rebuild switch --flake .#nixos
+
+./dotfiles/sync.sh
